4db521c7d5b61235d6a37d46f9ed6b3347b2bbe5
RYLE <urciaryle@gmail.com>
2021-02-07 09:51:13 -0500

    feat: Reusable Slider Component

---
+++ src/Components/SliderComponent.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+const SliderComponent = () => {
+
+    return(
+        <div>
+        </div>
+    )
+}
+
+
+export default SliderComponent;
