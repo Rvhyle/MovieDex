ef8691e33269e130079b096d50515eb2d7d1541e
RYLE <urciaryle@gmail.com>
2021-02-27 12:09:35 -0500

    refactor: Fetch API now returns the whole data array
    before the function would return a single object

--- src/Utils/FetchAPI.tsx
+++ src/Utils/FetchAPI.tsx
@@ -2,10 +2,10 @@ import axios from 'axios';
 import {APIKEY} from "./APIKey";
 
 
-const fetchMovieData = async(category : string, index : any) => {
+const fetchMovieData = async(category : string) => {
     try {
         let req =  await axios.get(`https://api.themoviedb.org/3/movie/${category}?api_key=${APIKEY}&language=en-US&page=1`);
-        return req.data.results[index]
+        return req.data.results
     }
     catch(e){
         console.log(e)
