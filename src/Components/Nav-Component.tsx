b5e4e6d32b2d1d95dca1e4b27d2a04b31bc70919
RYLE <urciaryle@gmail.com>
2021-02-27 12:12:40 -0500

    Simple changes/refactors

--- src/Components/Nav-Component.tsx
+++ src/Components/Nav-Component.tsx
@@ -3,7 +3,7 @@ import '../Styles/NavStyled.css';
 
 const NavComponent = () => {
     return (
-        <div className="nav-container flex sticky items-center  w-screen m-auto">
+        <div className="nav-container flex sticky items-center w-screen m-auto">
             <div className="flex items-center w-4/5 m-auto">
                 <h1 className="pr-16 font-extrabold text-3xl logo">Movie<span id="logospan">Dex</span></h1>
                 <ul className="flex gap-11 font-extrabold">
