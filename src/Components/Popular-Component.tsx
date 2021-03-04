53706622831e6cb8b56035c0e27155d4defa486a
RYLE <urciaryle@gmail.com>
2021-03-04 13:11:28 -0500

    feat: Slick Slides implemented -Auto plays

--- src/Components/Popular-Component.tsx
+++ src/Components/Popular-Component.tsx
@@ -1,23 +1,40 @@
 import React from 'react';
+import Slider from "react-slick";
+//Styles
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import '../Styles/SliderStyles.css'
 import '../Styles/PopularStyled.css';
-
 
 // @ts-ignore
 const PopularComponent = ({popular}) => {
+    const settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        draggable: true,
+        autoplay : true,
+        autoplaySpeed :4000
+    }
 
     //Reduces Array down to 6 Objects
     let reducedArray = [...popular].slice(0, 6)
 
     return (
         <div className="popular-container">
-            <h1>Popular Movies</h1>
+            <h1 className="popular-title">What's Popular</h1>
+            <Slider className="w-4/5 m-auto" {...settings}>
                 {[...reducedArray].map(movie => {
                     return (
                         <div key={movie.id}>
-                            <h1>{movie.title}</h1>
+                            <div className="slide-item">
+                                <img className="popular-poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title}/>
+                            </div>
                         </div>
                     )
                 })}
+            </Slider>
         </div>
     )
 }
