9d9007615654e63d9a6a902c1b5b8f3c5f7e507f
RYLE <urciaryle@gmail.com>
2021-03-05 16:56:48 -0500

    feat: New Movies Component

---
+++ src/Components/HomePage/NewMovies-Component.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import '../../Styles/NewMoviesStyled.css';
+import '../../Styles/poster-styled.css';
+
+//@ts-ignore
+const NewMoviesComponent = ({newMovie}) => {
+
+    let reducedArray : Array<any> = [...newMovie].slice(5,11);
+
+    return(
+        <div className="newMovies-container">
+            <h1 id="upcoming-title">Upcoming Movies</h1>
+            <div className="newMovies_poster-container flex flex-wrap justify-between">
+                {[...reducedArray].map((movie) => {
+                    return(
+                        <div className="movie-posters" key={movie.id}>
+                            <img className="poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title}/>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    )
+}
+
+
+export default NewMoviesComponent;
