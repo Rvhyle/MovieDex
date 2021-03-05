e59b959c72d2f199dd5e862c6b9bc2736a34eb03
RYLE <urciaryle@gmail.com>
2021-03-05 16:57:34 -0500

    refactor: Minor changes
    -Style paths changed
    -class name for img changed to 'poster'

---
+++ src/Components/HomePage/Popular-Component.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Slider from "react-slick";
+//Styles
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import '../../Styles/SliderStyles.css'
+import '../../Styles/PopularStyled.css';
+import '../../Styles/poster-styled.css';
+
+// @ts-ignore
+const PopularComponent = ({popular}) => {
+    const settings = {
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+        draggable: true,
+        autoplay: true,
+        autoplaySpeed: 3000
+    }
+
+    //Reduces Array down to 6 Objects
+    let reducedArray = [...popular].slice(0, 6);
+
+    return (
+        <div className="popular-container">
+            <h1 className="popular-title">What's Popular</h1>
+            <Slider className="w-4/5 m-auto" {...settings}>
+                {[...reducedArray].map(movie => {
+                    return (
+                        <div key={movie.id}>
+                            <div className="slide-item">
+                                <img className="poster" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+                                     alt={movie.title}/>
+                            </div>
+                        </div>
+                    )
+                })}
+            </Slider>
+        </div>
+    )
+}
+
+
+export default PopularComponent;
