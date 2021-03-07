f3cd5f8cc568d1debdf5e6bd8314b8ef9384d2ad
RYLE <urciaryle@gmail.com>
2021-03-07 15:52:04 -0500

    Style: Mobile View
    -Added Burger Menu

--- src/Components/HomePage/Nav-Component.tsx
+++ src/Components/HomePage/Nav-Component.tsx
@@ -1,18 +1,38 @@
-import React from 'react';
+import React, {useState} from 'react';
+import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 import '../../Styles/NavStyled.css';
 
 const NavComponent = () => {
 
+    let [isPressed,setPressed] = useState(false);
+
+    let menuOpen = () => {
+        setPressed(true);
+    }
+
+    let menuClose = () => {
+        setPressed(false);
+    }
+
     return (
-        <div className="nav-container flex sticky items-center w-full m-auto">
-                <div className="flex items-center w-4/5 m-auto">
-                    <h1 className="pr-16 font-extrabold text-3xl logo">Movie<span id="logospan">Dex</span></h1>
-                    <ul className="flex gap-11 font-extrabold">
-                        <li>Movies</li>
-                        <li>Shows</li>
-                    </ul>
-                </div>
+        <div className="nav-container sticky  w-full m-auto">
+            <div className="w-4/5 m-auto flex flex-wrap items-center nav_content-container ">
+                <h1 className="pr-16 font-extrabold text-3xl logo">Movie<span id="logospan">Dex</span></h1>
+                <button onClick={() => menuOpen()} className={!isPressed ? "openBurger" : "closeBurger"}><MenuIcon style={{ fontSize: 30 }} /></button>
+                <button onClick={() => menuClose()} className={isPressed ? "openBurger" : "closeBurger"}><CloseIcon style={{ fontSize: 30 }}/></button>
+                <ul className="flex gap-14 font-bold dv">
+                    <li>Movies</li>
+                    <li>Shows</li>
+                </ul>
             </div>
+
+            {/*Burger Nav*/}
+            <ul className={isPressed ? "open font-bold" : "close"}>
+                <li>Movies</li>
+                <li>Shows</li>
+            </ul>
+        </div>
         )
 
 }
--- src/Components/HomePage/TopRated_Component.tsx
+++ src/Components/HomePage/TopRated_Component.tsx
@@ -25,6 +25,21 @@ const TopRatedComponent = ({topMovies}) => {
                     slidesToShow: 2,
                     slidesToScroll: 3,
                     infinite: true,
+                    draggable: true,
+                    autoplay: true,
+                    autoplaySpeed: 3000
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    draggable: true,
+                    autoplay: true,
+                    infinite: true,
+                    autoplaySpeed: 3000,
+                    accessibility: false
                 }
             }]
     }
